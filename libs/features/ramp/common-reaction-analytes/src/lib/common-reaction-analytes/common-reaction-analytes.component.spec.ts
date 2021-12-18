56f4f361d315b78bb9dfe4a7ccd5ee8930335d80
tsheils <tksheils1@gmail.com>
2021-12-17 22:04:30 -0500

    tests for metabolites and reactions

--- libs/features/ramp/common-reaction-analytes/src/lib/common-reaction-analytes/common-reaction-analytes.component.spec.ts
+++ libs/features/ramp/common-reaction-analytes/src/lib/common-reaction-analytes/common-reaction-analytes.component.spec.ts
@@ -1,4 +1,13 @@
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ActivatedRoute} from "@angular/router";
+import {RouterTestingModule} from "@angular/router/testing";
+import {EffectsModule} from "@ngrx/effects";
+import {StoreModule} from "@ngrx/store";
+import {RampFacade, StoresRampStoreModule} from "@ramp/stores/ramp-store";
 
 import { CommonReactionAnalytesComponent } from './common-reaction-analytes.component';
 
@@ -8,7 +17,45 @@ describe('CommonReactionAnalytesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CommonReactionAnalytesComponent ]
+      declarations: [ CommonReactionAnalytesComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        StoresRampStoreModule,
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot(
+          {},
+          {
+            metaReducers: [],
+            runtimeChecks: {
+              strictActionImmutability: true,
+              strictStateImmutability: true
+            }
+          }
+        ),
+        EffectsModule.forRoot([]),
+      ],
+      providers: [
+        RampFacade,
+        {
+          provide: ActivatedRoute, useValue: {
+            snapshot: {
+              data: {
+                title: "Analytes From Pathways",
+                input: "pathways",
+                function: "analytes <- RaMP::getAnalyteFromPathway(pathway=\"###REPLACE###\")",
+                examples: "De Novo Triacylglycerol Biosynthesis, sphingolipid metabolism",
+                description: "Analytes (genes, proteins, metabolites) can be retrieve by pathway. Enter a list of exact pathway names."
+              }
+            }
+          }
+        }
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     })
     .compileComponents();
   });
--- libs/models/ramp-models/src/lib/metabolite.spec.ts
+++ libs/models/ramp-models/src/lib/metabolite.spec.ts
@@ -2,6 +2,10 @@ import { Metabolite } from './metabolite';
 
 describe('Metabolite', () => {
   it('should create an instance', () => {
-    expect(new Metabolite()).toBeTruthy();
+    expect(new Metabolite(
+      {
+        pathwayName: "tim"
+      }
+    )).toBeTruthy();
   });
 });
