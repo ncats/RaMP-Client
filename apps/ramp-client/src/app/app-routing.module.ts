7bf4dcf6de3294ef3da2ec179c07605fae933a98
tsheils <tksheils1@gmail.com>
2022-03-04 10:05:55 -0500

    fix angular universal flickering

--- apps/ramp-client/src/app/app-routing.module.ts
+++ apps/ramp-client/src/app/app-routing.module.ts
@@ -144,7 +144,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
+    initialNavigation: 'enabledNonBlocking'
 })],
   exports: [RouterModule],
 })
