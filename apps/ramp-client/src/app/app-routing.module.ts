be72a2cfc39c2ebe00c01e33771afab424735cb0
Tim Sheils <tksheils1@gmail.com>
2022-03-04 09:38:34 -0500

    Update app-routing.module.ts

--- apps/ramp-client/src/app/app-routing.module.ts
+++ apps/ramp-client/src/app/app-routing.module.ts
@@ -143,7 +143,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), {
+    initialNavigation: 'enabledNonBlocking'
+}],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
