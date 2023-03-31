029da0e94a41ec1e7ed4fb8fc6828a227da3edeb
johnbraisted <jb212828@gmail.com>
2023-03-31 12:21:48 -0400

    update for test RaMP-DB branch

--- apps/ramp-server/install.R
+++ apps/ramp-server/install.R
@@ -1,4 +1,4 @@
 library(devtools)
-install_github("ncats/RaMP-DB", ref="RdataUpdate_2.2.1", force = TRUE, dependencies=TRUE)
+install_github("ncats/RaMP-DB", ref="pw_enrich_dev", force = TRUE, dependencies=TRUE)
 library(RaMP)
 
