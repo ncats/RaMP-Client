78ea8c13a572b8b0f0ad264c6735e0920f7e175a
johnbraisted <jb212828@gmail.com>
2023-10-30 15:06:20 -0400

    test_on_sqlite_branch

--- apps/ramp-server/install.R
+++ apps/ramp-server/install.R
@@ -1,6 +1,9 @@
 library(devtools)
 
-install_github("ncats/RaMP-DB", force = TRUE, dependencies=TRUE)
+#install_github("ncats/RaMP-DB", force = TRUE, dependencies=TRUE)
+
+# test sqlite branch
+install_github("ncats/RaMP-DB@sqlite", force = TRUE, dependencies=TRUE)
 
 library(RaMP)
 
